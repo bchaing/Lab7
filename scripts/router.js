c01359524fadfae04af8bd3bd3a08753a2d86d2c
Brandon Chaing <bchaing@ucsd.edu>
2021-05-16 23:42:37 -0700

    Fixed states being pushed to history when on page

--- scripts/router.js
+++ scripts/router.js
@@ -40,7 +40,7 @@ router.setState = function (state, back) {
   let heading = document.querySelector("h1");
   let url;
 
-  console.log(history.state);
+  if (history.state.name == state.name && !back) return;
 
   switch (state.name) {
     case "home":
